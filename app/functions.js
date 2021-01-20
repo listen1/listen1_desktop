cc63827d79beb8e294e76a16d5ad94a5f596194a
Xiaokai Li <mike.lxk@gmail.com>
2021-01-20 19:17:41 +0800

    update returned tag data format - update returned tag data format - add  space between switch case

--- app/functions.js
+++ app/functions.js
@@ -1,22 +1,17 @@
 const mm = require("music-metadata");
 const path = require("path");
 module.exports = {
-  readAudioTags(filePath) {
-    return new Promise((resolve, reject) => {
-      let tag = {};
-      tag.tags = {
+  async readAudioTags(filePath) {
+    try {
+      return await mm.parseFile(filePath);
+    } catch (error) {
+      let metadata = {};
+      metadata.common = {
         title: path.basename(filePath, path.extname(filePath)),
         artist: "",
         album: "",
       };
-      mm.parseFile(filePath)
-        .then((metaData) => {
-          tag.tags = metaData.common;
-          resolve(tag);
-        })
-        .catch(() => {
-          resolve(tag);
-        });
-    });
+      return metadata;
+    }
   },
 };
