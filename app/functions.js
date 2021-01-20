645d8c0067b20733b17b7e5a6507bed726cf70a1
Xiaokai Li <mike.lxk@gmail.com>
2021-01-20 23:26:43 +0800

    get a default title when it doesn't exists

--- app/functions.js
+++ app/functions.js
@@ -2,13 +2,18 @@ const mm = require("music-metadata");
 const path = require("path");
 module.exports = {
   async readAudioTags(filePath) {
+    const fileName = path.basename(filePath, path.extname(filePath));
     try {
-      return await mm.parseFile(filePath);
+      const metaData = await mm.parseFile(filePath);
+      if (!metaData.common.title) {
+        metaData.common.title = fileName;
+      }
+      return metaData;
     } catch (error) {
       console.warn(error);
       return {
         common: {
-          title: path.basename(filePath, path.extname(filePath)),
+          title: fileName,
           album: "",
           artist: "",
         },
