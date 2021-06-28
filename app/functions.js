5220272af0b5cc8f1211278187fe3e5972deb799
Xiaokai Li <mike.lxk@gmail.com>
2021-06-28 22:39:34 +0800

    build: update deps

--- app/functions.js
+++ app/functions.js
@@ -1,14 +1,11 @@
 const { parseFile } = require("music-metadata");
 const { basename, extname } = require("path");
-const fs = require("fs");
 module.exports = {
   async readAudioTags(filePath) {
     const fileName = basename(filePath, extname(filePath));
     try {
       const metaData = await parseFile(filePath);
-      if (!metaData.common.title) {
-        metaData.common.title = fileName;
-      }
+      metaData.common.title ||= fileName;
       return metaData;
     } catch (error) {
       console.warn(error);
